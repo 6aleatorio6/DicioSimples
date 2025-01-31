676cdab45d86e23700459824caf5a734be367bb4
leonardo <leonardolfelix12@gmail.com>
2025-01-31 01:10:04 -0300

    🖥️ feat(tailwind): adicionar cores personalizadas e fontes ao tema

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,5 +1,5 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,8 +12,18 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                ...defaultTheme.colors,
+                body: '#E5E7EB', // gray-200 in hex
+                bodySec: '#C8B798',
+                bodyTri: '#88A5BF',
+                text: '#232A33',
+                textSec: '#324E73',
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                Jomhuria: ['Jomhuria', ...defaultTheme.fontFamily.sans], // Adiciona a fonte Johuria,
+                inter: ['Inter', ...defaultTheme.fontFamily.sans], // Adiciona a fonte Inter,
             },
         },
     },
