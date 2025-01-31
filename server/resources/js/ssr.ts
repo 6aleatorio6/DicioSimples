713e4d231f0262bf546ac13ca482bc244b8dac37
leonardo <leonardolfelix12@gmail.com>
2025-01-31 01:09:50 -0300

    🖥️ feat(layout): criar layout para pagina e adicionar ícones de redes sociais

--- server/resources/js/ssr.ts
+++ server/resources/js/ssr.ts
@@ -4,6 +4,7 @@ import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createSSRApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import VisitorLayout from './Layouts/VisitorLayout.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -12,11 +13,17 @@ createServer((page) =>
         page,
         render: renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) =>
-            resolvePageComponent(
+        resolve: async (name) => {
+            const pages = await resolvePageComponent(
                 `./Pages/${name}.vue`,
                 import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
-            ),
+            );
+
+            if (name.startsWith('Public'))
+                pages.default.layout ??= VisitorLayout;
+
+            return pages;
+        },
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
