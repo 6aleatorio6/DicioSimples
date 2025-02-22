99873d91e32ec436b09ae544fe5043e3a15334ba
leonardo <leonardolfelix12@gmail.com>
2025-02-22 00:43:37 -0300

    🖥️ feat(routes, controller): adicionar WordContentController e rota para exibir conteúdo das palavras

---
+++ server/app/Http/Controllers/WordContentController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Word;
+use App\Services\WordContentGeneratorService;
+use App\Services\WordSuggestionService;
+use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+
+class WordContentController extends Controller
+{
+    function __construct(
+        private WordContentGeneratorService $wordMeaningGeneratorService,
+        private WordSuggestionService $wordSuggestionService,
+        private Word $word
+    ) {}
+
+
+
+    private function getWordContent(string $wordName)
+    {
+        $wordContent = $this->word->with(['baseForm:word', 'wordSynonyms:id,word', 'wordAntonyms:id,word'])
+            ->where('word', '=', $wordName)
+            ->whereNotNull('meanings')
+            ->first();
+
+
+        if (!$wordContent) {
+            $wordContentGenereated = $this->wordMeaningGeneratorService->generate($wordName);
+
+            $wordContent = $this->word->updateOrCreate(['word' => $wordName], $wordContentGenereated);
+
+            $wordContent->createAndAttachWords("Antonyms", $wordContentGenereated['antonyms']);
+            $wordContent->createAndAttachWords("Synonyms", $wordContentGenereated['synonyms']);
+            $wordContent->baseForm()->associate($this->word->firstOrcreate(['word' => $wordContentGenereated['wordBase']]));
+
+            $wordContent->load(['baseForm:word', 'wordSynonyms:id,word', 'wordAntonyms:id,word']);
+        }
+
+
+        return $wordContent;
+    }
+
+
+    public function __invoke(string $word)
+    {
+        // Validate word
+        $isWordValid = $this->wordSuggestionService->isWordValid($word);
+        if (!$isWordValid) throw new BadRequestException('Palavra inválida');
+
+        $word = $this->getWordContent($word);
+
+        return Inertia::render('public/WordContent', $word);
+    }
+}
--- server/routes/web.php
+++ server/routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WordContentController;
 use App\Http\Controllers\WordSearchController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', [WordSearchController::class, 'show'])->name('home');
 Route::post('/', [WordSearchController::class, 'search'])->name('home.search');
+
+Route::get('/words/{word}', WordContentController::class)->name('word');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
