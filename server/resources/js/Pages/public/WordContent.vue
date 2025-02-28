8821cf329df56f536afc90db10fea8451c46819d
leonardo <leonardolfelix12@gmail.com>
2025-02-28 20:15:06 -0300

    🖥️ feat(pages): Melhorar a página WordContent

--- server/resources/js/Pages/public/WordContent.vue
+++ server/resources/js/Pages/public/WordContent.vue
@@ -1,11 +1,9 @@
 <script lang="ts" setup>
-import ListVertical from '@/Components/List.vue';
+import List from '@/Components/List.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import SectionContent from '@/Components/SectionContent.vue';
+import { WordRelation } from '@/types';
 import { Head, Link } from '@inertiajs/vue3';
-
-interface WordRelation {
-    word: string;
-    id: number;
-}
 
 defineProps<{
     word: string;
@@ -22,11 +20,11 @@ function capWord(word: string): string {
 
 <template>
     <Head :title="capWord(word)" />
-    <section class="mt-7">
-        <h1 class="ms- text-4xl font-bold uppercase">{{ word }}</h1>
+    <section class="mt-8">
+        <h1 class="text-5xl font-bold uppercase">{{ word }}</h1>
         <div class="my-1 border-b-2 border-gray-400" />
-        <p v-if="baseForm" class="ms-4 text-lg">
-            Forma base:
+        <p v-if="baseForm && baseForm.word !== word" class="ms-4 text-lg">
+            Palavra base:
             <Link :href="route('word', baseForm.word)">
                 <span class="text-blue-800 underline">{{
                     capWord(baseForm.word)
@@ -34,36 +32,42 @@ function capWord(word: string): string {
             </Link>
         </p>
     </section>
-    <section class="mt-4">
+
+    <SectionContent title="Definições">
         <div
             v-for="(meaning, index) in meanings"
             :key="index"
-            class="ms-3 mt-2 text-lg"
+            class="mt-2 text-lg"
         >
-            <strong>{{ index + 1 }}. {{ meaning.title }} - </strong>
-            {{ meaning.explanation }}
+            <h3 class="inline font-bold">
+                {{ index + 1 }}. {{ meaning.title }}
+            </h3>
+            <span> – </span>
+            <p class="inline">{{ meaning.explanation }}</p>
         </div>
-    </section>
-    <section class="mt-4 flex">
-        <ListVertical
-            v-if="wordSynonyms.length"
-            title="Palavras com sentido semelhante"
+    </SectionContent>
+
+    <SectionContent title="Sinônimos">
+        <List
+            startText="Palavras com significado semelhante: "
+            emptyListText="Nenhum sinônimo disponível para esta palavra"
             :listWord="wordSynonyms"
+            title="Sinônimos"
         />
-        <div
-            v-if="wordSynonyms.length && wordAntonyms.length"
-            class="mx-4 border-l-2 border-gray-300"
-        ></div>
-        <ListVertical
-            v-if="wordAntonyms.length"
-            title="Palavras com sentido contrário"
+    </SectionContent>
+
+    <SectionContent title="Antônimos">
+        <List
+            startText="Palavras com significado oposto: "
+            emptyListText="Nenhum antônimo disponível para esta palavra"
             :listWord="wordAntonyms"
+            title="Antônimos"
         />
-    </section>
-    <Link
-        :href="route('home')"
-        class="rounded bg-bodyTri px-4 py-1 text-xl font-bold"
-    >
-        Voltar
+    </SectionContent>
+
+    <Link :href="route('home')" class="m-auto w-8/12">
+        <PrimaryButton class="h-14 w-full justify-center text-2xl">
+            Buscar outra palavra
+        </PrimaryButton>
     </Link>
 </template>
