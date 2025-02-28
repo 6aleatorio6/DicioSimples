4ec0e0cc4e8618b3cb612f6e4244210df73714b4
leonardo <leonardolfelix12@gmail.com>
2025-02-28 20:13:05 -0300

    🖥️ feat(components): Refatoar o List e criar componente para abrigar as seções da página WordContent

---
+++ server/resources/js/Components/SectionContent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+    <section class="mt-4">
+        <h2 class="text-xl font-semibold">{{ title }}</h2>
+        <div class="ms-3">
+            <slot />
+        </div>
+    </section>
+</template>
