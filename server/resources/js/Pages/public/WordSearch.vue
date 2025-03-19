31bf5ca4c751c756c1c35e784c80c59e16cf75e9
leonardo <leonardolfelix12@gmail.com>
2025-03-19 01:16:24 -0300

    refactor(pages/WordSearch): ajustar espaçamento e descrição na página de busca de palavras

--- server/resources/js/Pages/public/WordSearch.vue
+++ server/resources/js/Pages/public/WordSearch.vue
@@ -50,17 +50,17 @@ const moveSelected = (distance: number) => {
 
 <template>
     <div
-        class="mx-auto mt-[8%] flex flex-col items-center p-4 sm:w-10/12 lg:w-8/12"
+        class="mx-auto mt-[25%] flex flex-col items-center p-4 sm:mt-[10%] sm:w-10/12 lg:w-8/12"
     >
         <Head>
             <title>Inicio</title>
             <meta
                 name="description"
-                content="No DicioSimples, pesquise palavras e encontre definições claras e objetivas de forma rápida e prática."
+                content="Pesquise palavras e obtenha explicações claras e objetivas de forma rápida e prática, com antônimos, sinônimos e descrições detalhadas dos diversos significados."
             />
         </Head>
         <div
-            class="mb-4 flex w-1/2 items-baseline justify-center space-x-2 pb-4"
+            class="mb-4 flex w-1/3 items-baseline justify-center space-x-2 sm:pb-4"
         >
             <ApplicationLogo width="45%" />
             <ApplicationLogo width="80%" />
