54107ce45540f635451a376e52f1e80dd5eaa341
leonardo <leonardolfelix12@gmail.com>
2025-03-20 15:02:03 -0300

    refactor(tailwind): atualizar paleta de cores para melhor contraste e harmonização

--- server/resources/js/Components/PrimaryButton.vue
+++ server/resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="bg-bodyTri-800 hover:bg-bodyTri-700 focus:bg-bodyTri-700 active:bg-bodyTri-900 text-textSec-100 inline-flex items-center rounded-md border border-transparent px-4 py-2 text-xs font-semibold uppercase tracking-widest transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        class="inline-flex items-center rounded-md border border-transparent bg-bodyTri-700 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-textSec-200 transition duration-150 ease-in-out hover:bg-bodyTri-400 focus:bg-bodyTri-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-bodyTri-500"
     >
         <slot />
     </button>
