ac74c969c7973336318ad4edaa73bf50c445bb4e
leonardo <leonardolfelix12@gmail.com>
2025-03-24 00:30:13 -0300

    fix: substituir todos os lugares que usavam o name 'dashboard'

--- server/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ server/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -14,7 +14,7 @@ class EmailVerificationNotificationController extends Controller
     public function store(Request $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false));
+            return redirect()->intended(route('admin.words.index', absolute: false));
         }
 
         $request->user()->sendEmailVerificationNotification();
